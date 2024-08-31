5da21a9b2abfb9427a58d66c7349493ce0431334
IFreshman <freshkreiner@gmail.com>
2024-08-31 17:21:00 +0200

    Add Prettier Config, Product-Pictures, etc.

--- src/components/base/BaseRadioGroup.vue
+++ src/components/base/BaseRadioGroup.vue
@@ -1,28 +1,18 @@
 <script setup lang="ts">
-
 const props = defineProps<{
-  items: Array<{ value: string; label?: string; disabled?: boolean }>;
+  items: Array<{ value: string; label?: string; disabled?: boolean }>
   modelValue: string
-  selectColor?: string;
-}>();
+  selectColor?: string
+}>()
 
 const emit = defineEmits<{
-    "update:modelValue": [value: string]
-}>();
-
+  "update:modelValue": [value: string]
+}>()
 </script>
 
-<template> 
-    <div v-for="(item, index) in props.items" :key="index">
-    <input
-      type="radio"
-      class="hidden"
-      :id="item.value"
-      :value="item.value"
-      :disabled="item.disabled"
-      :checked="modelValue === item.value"
-      @change="$emit('update:modelValue', item.value)"
-    />
+<template>
+  <div v-for="(item, index) in props.items" :key="index">
+    <input type="radio" class="hidden" :id="item.value" :value="item.value" :disabled="item.disabled" :checked="modelValue === item.value" @change="$emit('update:modelValue', item.value)" />
     <label :for="item.value">
       <slot name="label" :item="item">{{ item.label }}</slot>
     </label>
--- src/modules/products/components/view/Dialog.vue
+++ src/modules/products/components/view/Dialog.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import { productsAPI } from "../../services/products.api";
-import BaseRadioGroup from "../../../../components/base/BaseRadioGroup.vue";
-import { Size } from "../../types/product";
+import { computed, ref } from "vue"
+import { productsAPI } from "../../services/products.api"
+import BaseRadioGroup from "../../../../components/base/BaseRadioGroup.vue"
+import { Size } from "../../types/product"
+import Tab from "../../../../components/base/TabView.vue"
 
-const selectedColor = ref("");
-const selectedSize = ref("");
-const [error, product] = await productsAPI.getProduct(1);
+const selectedColor = ref("")
+const selectedSize = ref("")
+const [error, product] = await productsAPI.getProduct(1)
 
 if (error) {
-  console.error("Failed to fetch the product:", error);
+  console.error("Failed to fetch the product:", error)
 }
 
 const colorItems = computed(() => {
@@ -17,53 +18,55 @@ const colorItems = computed(() => {
     value: color.value,
     label: color.name,
     disabled: !color.sizes.some((size) => size.available),
-  }));
-});
+  }))
+})
 
 const sizeItems = computed(() => {
-  const selectedColors = product?.color.find(
-    (c) => c.value === selectedColor.value,
-  );
+  const selectedColors = product?.color.find((c) => c.value === selectedColor.value)
   return selectedColors
     ? selectedColors.sizes.map((size: Size) => ({
         value: size.size,
         label: size.size,
         disabled: !size.available,
       }))
-    : [];
-});
+    : []
+})
 
 function getColorImage(value: string) {
-  return new URL(
-    `/src/assets/clothes/Ltwre&asyt/Color/${value}.webp`,
-    import.meta.url,
-  ).href;
+  return new URL(`/src/assets/clothes/Ltwre&asyt/Color/${value}.webp`, import.meta.url).href
 }
 </script>
 
 <template>
   <div v-if="product">
-    <p>{{ product }}</p>
     <span class="font-normal">Color:</span> {{ selectedColor }}
-    <BaseRadioGroup :items="colorItems" v-model="selectedColor">
-      <template #label="{ item }">
-        <img
-          class="h-14 w-14 rounded-full object-contain"
-          :class="{
-            'outline outline-2 outline-gray-800': selectedColor == item.value,
-          }"
-          :src="getColorImage(item.value)"
-          :alt="item.value"
-        />
-      </template>
-    </BaseRadioGroup>
+    <div class="flex gap-4">
+      <BaseRadioGroup :items="colorItems" v-model="selectedColor">
+        <template #label="{ item }">
+          <img
+            class="h-14 w-14 rounded-full object-contain"
+            :class="{
+              'outline outline-2 outline-gray-800': selectedColor == item.value,
+            }"
+            :src="getColorImage(item.value)"
+            :alt="item.value"
+          />
+        </template>
+      </BaseRadioGroup>
+    </div>
 
     <!-- Idea: wenn out of stock/ disabled then add sth-->
     <span class="font-normal">Size:</span> {{ selectedSize }}
-    <BaseRadioGroup :items="sizeItems" v-model="selectedSize" :select-color="selectedColor">
-      <template #label="{ item }"> 
-        <div>{{ item.value }}</div>
-      </template>
-    </BaseRadioGroup>
+    <div class="flex gap-2">
+      <BaseRadioGroup :items="sizeItems" v-model="selectedSize" :select-color="selectedColor">
+        <template #label="{ item }">
+          <div>{{ item.value }}</div>
+        </template>
+      </BaseRadioGroup>
+    </div>
+
+    <div>
+      <Tab></Tab>
+    </div>
   </div>
 </template>
